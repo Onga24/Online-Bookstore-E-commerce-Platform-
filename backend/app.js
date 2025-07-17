270b6cb4e5a72d40b475bc7ad2c7f165ed2be759
Abdullah-Arfa <abdallah.arfa09@gmail.com>
2025-07-17 22:23:33 +0300

    handle book retrivement api - from db

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,31 @@
- const express = require("express");
+//  const express = require("express");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+// const mongoose = require("mongoose");
+
+// // this file is used to set up the server, connect to the database, and define routes
+// // It imports necessary modules, sets up middleware, and starts the server on a specified port.
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// const orderRoutes = require("./routes/orderRoutes");
+// app.use("/api/orders", orderRoutes);
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`)); 
+
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const mongoose = require("mongoose");
-
+const bookRoutes = require('./routes/book.routes');
 // this file is used to set up the server, connect to the database, and define routes
 // It imports necessary modules, sets up middleware, and starts the server on a specified port.
 dotenv.config();
@@ -13,11 +35,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const orderRoutes = require("./routes/orderRoutes");
-app.use("/api/orders", orderRoutes);
+app.use('/api/books', bookRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`)); 
-
+app.listen(PORT, () => console.log('🚀 Server running on port ${PORT}'));
