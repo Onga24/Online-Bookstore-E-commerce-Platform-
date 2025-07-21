e475d5ed8fd42a644cc8e8d15f308a48d7ddd8f8
Mohamed <nasrmohamed575@gmail.com>
2025-07-21 10:36:11 +0300

    edit on pagination backend

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -1,33 +1,3 @@
-// const Book = require("../models/book.model");
-// // this file is used to handle book-related operations, such as adding a new book and retrieving all books.
-// exports.addBook = async (req, res) => {
-//   try {
-//     const { title, author, description, category, price, pdfUrl, coverImage } = req.body;
-//     const newBook = new Book({
-//       title,
-//       author,
-//       description,
-//       category,
-//       price,
-//       pdfUrl,
-//       coverImage
-//     });
-
-//     await newBook.save();
-//     res.status(201).json({ message: "Book added successfully", book: newBook });
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// };
-
-// exports.getAllBooks = async (req, res) => {
-//   try {
-//     const books = await Book.find();
-//     res.status(200).json(books);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// };
 const Book = require("../models/book.model");
 // this file is used to handle book-related operations, such as adding a new book and retrieving all books.
 exports.addBook = async (req, res) => {
@@ -50,11 +20,21 @@ exports.addBook = async (req, res) => {
   }
 };
 
+
+
+// GET /api/books?page=1&limit=8
 exports.getAllBooks = async (req, res) => {
+
   try {
-    const books = await Book.find();
-    res.status(200).json(books);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 8;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find().skip(skip).limit(limit);
+    const totalCount = await Book.countDocuments();
+
+    res.status(200).json({ books, totalCount });
   } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    res.status(500).json({ error: err.message });
+  }
 };
--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -6,11 +6,11 @@
 // router.get("/", bookController.getAllBooks);
 
 // module.exports = router;
-const express = require("express");
-const router = express.Router();
-const bookController = require("../controllers/book.controller");
-// This file defines the routes for book-related operations, such as adding a new book and retrieving all books.
-router.post("/add", bookController.addBook);
-router.get("/", bookController.getAllBooks);
+    const express = require("express");
+    const router = express.Router();
+    const bookController = require("../controllers/book.controller");
+    // This file defines the routes for book-related operations, such as adding a new book and retrieving all books.
+    router.post("/add", bookController.addBook);
+    router.get("/", bookController.getAllBooks);
 
-module.exports = router;
+    module.exports = router;
