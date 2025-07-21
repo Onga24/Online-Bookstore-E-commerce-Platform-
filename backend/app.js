4a21f45c98df620e7c00bee8ace31b636e7eb895
MohamedAmgad2001 <mohamedamgadm.2001@gmail.com>
2025-07-21 05:14:47 +0300

    Complete merge of auth into main

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,48 @@
-//  const express = require("express");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const connectDB = require("./config/db");
-// const mongoose = require("mongoose");
-
-// // this file is used to set up the server, connect to the database, and define routes
-// // It imports necessary modules, sets up middleware, and starts the server on a specified port.
-// dotenv.config();
-// connectDB();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// const orderRoutes = require("./routes/orderRoutes");
-// app.use("/api/orders", orderRoutes);
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`)); 
-
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
-const bookRoutes = require('./routes/book.routes');
-const orderRoutes = require("./routes/orderRoutes");
-const mongoose = require("mongoose");
-// this file is used to set up the server, connect to the database, and define routes
-// It imports necessary modules, sets up middleware, and starts the server on a specified port.
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import userRouter from "./src/routers/user.router.js";
+import { dbConnect } from "./src/configs/database.config.js";
+import cookieparser from "cookie-parser";
+import passport from "passport";
+import session from "express-session";
+import { passportFunctionality } from "./src/configs/passport.config.js";
 dotenv.config();
-connectDB();
+dbConnect();
+// const express = require("express");
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
-app.use('/api/books', bookRoutes);
-app.use("/api/orders", orderRoutes);
+app.use(cookieparser());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+  })
+);
+passportFunctionality();
+// app.use(
+//   session({
+//     secret: process.env.JWT_SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: false, // true only with HTTPS
+//       httpOnly: true,
+//       maxAge: 1000 * 60 * 60 * 24, // 1 day
+//     },
+//   })
+// );
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('🚀 Server running on port ${PORT}'));
+// ✅ Initialize passport and session
+app.use(passport.initialize());
+// app.use(passport.session());
+const port = 5000;
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+app.use("/api/users", userRouter);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
